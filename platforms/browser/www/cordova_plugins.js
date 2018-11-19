2e3b4d401ea549fc9819f9d775e4fe81c1f7a768
appgenny <appgenerator2@gmail.com>
2018-11-19 19:54:54 +0500

    asdasdasd

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,21 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "pluginId": "cordova-plugin-admobpro",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-android-play-services-gradle-release": "2.0.0",
+    "cordova-plugin-extension": "1.5.4",
+    "cordova-plugin-admobpro": "2.35.3",
+    "cordova-plugin-whitelist": "1.3.3"
+}
+// BOTTOM OF METADATA
+});
